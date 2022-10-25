9162d8ac3052c28ab215ded50d1eb93f868a38de
Nataliia Shkarupeta <natalinardi.kh@gmail.com>
2022-10-25 09:41:28 +0300

    add button mobile

--- js/menu.js
+++ js/menu.js
@@ -7,6 +7,7 @@
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
   
       menuBtnRef.classList.toggle("is-open");
+      menuBtnRef.classList.toggle("is-fixed");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
   
       mobileMenuRef.classList.toggle("is-open");
